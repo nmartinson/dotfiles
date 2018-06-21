2c3b6056d1980d6de4cbafb8cf850d36c4833b05
Alexis Ortega <alexis.ortega@wwt.com>
2018-06-21 14:30:30 -0600

    Add git ignore file

--- git/install.sh
+++ git/install.sh
@@ -7,5 +7,6 @@ DOTFILES_DIR=~/.dotfiles
 
 export PATH="$DOTFILES_DIR/bin:$PATH"
 
+ln -sfv "$DOTFILES_DIR/git/.gitignore" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
