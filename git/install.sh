110e373683607d187d9695346e2548cf28e37aa1
Alexis Ortega <alexis.oc3@gmail.com>
2018-06-21 14:45:22 -0600

    Revert "Add git ignore file"
    This reverts commit 2c3b6056d1980d6de4cbafb8cf850d36c4833b05.

--- git/install.sh
+++ git/install.sh
@@ -7,6 +7,5 @@ DOTFILES_DIR=~/.dotfiles
 
 export PATH="$DOTFILES_DIR/bin:$PATH"
 
-ln -sfv "$DOTFILES_DIR/git/.gitignore" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
