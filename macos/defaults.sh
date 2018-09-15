c61331eb87c0f3167e2b1dcd26f2c89ce7e2b4ab
Alexis Ortega <alexis.oc3@gmail.com>
2018-09-15 14:25:32 -0600

    Update mac os defaults script (Tweetbot)

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -906,6 +906,13 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 # defaults write com.twitter.twitter-mac HideInBackground -bool true
 
 ###############################################################################
+# Tweetbot.app                                                                #
+###############################################################################
+
+# Bypass the annoyingly slow t.co URL shortener
+# defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
+
+###############################################################################
 # Do some clean up work.
 ###############################################################################
 
