442b6207fe7f69178f7fc51802ae53bfde69528c
Alexis Ortega <alexis.oc3@gmail.com>
2018-09-19 21:11:23 -0600

    Update install bash script

--- bash/install.sh
+++ bash/install.sh
@@ -4,10 +4,7 @@ set -e
 
 echo "==> Setting up bash..."
 
-export DOTFILES_DIR
 DOTFILES_DIR=~/.dotfiles
-
-export PATH="$DOTFILES_DIR/bin:$PATH"
 
 ln -sfv "$DOTFILES_DIR/bash/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/bash/.bash_profile" ~
