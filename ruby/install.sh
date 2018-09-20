1cfdcb7f6f234482e64bb84d41ac7b8ed4909a9c
Alexis Ortega <alexis.oc3@gmail.com>
2018-09-19 21:32:26 -0600

    Clean log messsages

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,24 +2,30 @@
 
 set -e
 
+echo "==> Setting up rvm..."
+
 if test ! $(which rvm); then
-    echo "==> Installing rvm"
+    echo "Installing rvm"
     \curl -L https://get.rvm.io | bash -s stable --auto-dotfiles
     source ~/.rvm/scripts/rvm
 else
-    echo "==> rvm is already installed"
+    echo "rvm is already installed"
 fi
+
+echo "==> Setting up ruby..."
 
 if test ! $(which ruby); then
-    echo "==> Installing ruby"
+    echo "Installing ruby"
     rvm use ruby --install --default
 else
-    echo "==> ruby is already installed"
+    echo "ruby is already installed"
 fi
 
+echo "==> Setting up rails..."
+
 if test ! $(which rails); then
-    echo "==> Installing rails"
+    echo "Installing rails"
     gem install rails --no-ri --no-rdoc
 else
-    echo "==> rails is already installed"
+    echo "rails is already installed"
 fi
--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,7 +1,7 @@
 ls ~/.oh-my-zsh &> /dev/null
 if [ $? -ne 0 ]; then
-    echo "==> Installing oh my zsh"
+    echo "Installing oh my zsh"
     0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
-    echo "==> oh my zsh is already installed"
+    echo "oh my zsh is already installed"
 fi
