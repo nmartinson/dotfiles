2ed023cb1bd379a19bf718f26e7b9088208c4c18
Nick Martinson <nickmartinson986@gmail.com>
2019-11-21 16:43:47 -0700

    Change dock apps

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,13 +4,13 @@ echo "==> Setting up your dock..."
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Android Studio.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Android Messages.app"
+dockutil --no-restart --add "/Applications/Adobe Lightroom Classic.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/iTunes.app"
-dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "~" --section others
+#dockutil --no-restart --add "~" --section others
 
 killall Dock
